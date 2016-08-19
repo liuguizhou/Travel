9b547fbeced99280f9f76e03ff499418882a736a
15256298062 <lgz_togoodluck@163.com>
2016-08-19 17:57:31 +0800

    08/19

--- app/src/main/java/com/travel/liuyun/fragment/SceneFragment.java
+++ app/src/main/java/com/travel/liuyun/fragment/SceneFragment.java
@@ -1,17 +1,40 @@
 package com.travel.liuyun.fragment;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.OrientationHelper;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.travel.liuyun.R;
+import com.travel.liuyun.adapter.ImageAdapter;
+
+import java.util.ArrayList;
+
+import droidninja.filepicker.FilePickerBuilder;
+import droidninja.filepicker.FilePickerConst;
 
 /**
  * Created by liuguizhou on 2016/5/1.
  */
 public class SceneFragment extends BaseFragment {
     private View rootView;
+    private ArrayList<String> filePaths;
+    private RecyclerView recyclerView;
+    private  ImageAdapter imageAdapter;
+    private Button button_photo;
+    private Button button_doc;
 
+    public SceneFragment() {
+        // Required empty public constructor sometimes.
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,7 +43,16 @@ public class SceneFragment extends BaseFragment {
 
     @Override
     protected void initView(View view, Bundle savedInstanceState) {
-
+        button_doc = (Button) view.findViewById(R.id.pick_doc);
+        button_photo = (Button) view.findViewById(R.id.pick_photo);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerview);
+        recyclerView.setHasFixedSize(true);
+        StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(3, OrientationHelper.VERTICAL);
+        layoutManager.setGapStrategy(StaggeredGridLayoutManager.GAP_HANDLING_MOVE_ITEMS_BETWEEN_SPANS);
+        recyclerView.setLayoutManager(layoutManager);
+        imageAdapter = new ImageAdapter();
+        recyclerView.setAdapter(imageAdapter);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
     }
 
     @Override
@@ -40,7 +72,48 @@ public class SceneFragment extends BaseFragment {
     }
 
     private void initListener() {
+        button_photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FilePickerBuilder.getInstance().setMaxCount(6)
+                        .setSelectedFiles(filePaths)
+                        .setActivityTheme(R.style.AppTheme)
+                        .pickPhoto(getActivity());
+            }
+        });
+        button_doc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FilePickerBuilder.getInstance().setMaxCount(12)
+                        .setSelectedFiles(filePaths)
+                        .setActivityTheme(R.style.AppTheme)
+                        .pickDocument(getActivity());
+            }
+        });
+    }
 
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.e("lgz", "filePaths = "+ filePaths.size());
+        switch (requestCode) {
+            case FilePickerConst.REQUEST_CODE:
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    filePaths = data.getStringArrayListExtra(FilePickerConst.KEY_SELECTED_PHOTOS);
+                    addThemToView(filePaths);
+                    Log.e("lgz", "filePaths = "+ filePaths.size());
+                }
+        }
+    }
+
+    private void addThemToView(ArrayList<String> filePaths) {
+        if (recyclerView != null) {
+            imageAdapter.setColumnNumber(getActivity(),3);
+            imageAdapter.addData(filePaths);
+        }
+        Log.e("lgz", "addThemToView");
+        Toast.makeText(getActivity(), "Num of files selected: " + filePaths.size(), Toast.LENGTH_SHORT).show();
     }
 
     @Override
